ba0f14d1532c62e4e138c3d51f7b15d498467c0f
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-03-01 19:21:52 +0100

    Ride picking logic.

--- rides.py
+++ rides.py
@@ -1,6 +1,7 @@
 class Car():
-    def __init__(self):
-        pass
+    def __init__(self, id):
+        self.id = id
+        self.position = (0,0)
 
 class Ride():
     def __init__(self, id, start_x, start_y, dest_x, dest_y, earliest, latest):
@@ -11,6 +12,9 @@ class Ride():
         self.dest_y = dest_y
         self.earliest = earliest
         self.latest = latest
+        self.completed = False
+        self.car = None
+        self.score = 0
 
     def __str__(self):
         return '[{}] from {},{} to {},{}'.format(self.id, self.start_x, self.start_y, self.dest_x, self.dest_y)
@@ -23,7 +27,22 @@ def parse_input(file):
             rides.append(Ride(i, *(line.split(' '))))
     return rides, rows, cols, n_vehicles, bonus, t
 
+def get_distance(start, end):
+    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+def score_ride(car, ride):
+    return 1
+
+def pick_ride(car, rides):
+    filtered = [r for r in rides if not r.completed and r.car is None]
+    return sorted(filtered, key=lambda r: score_ride(car, r), reverse=True)[0]
 
 if __name__ == '__main__':
     rides, rows, cols, n_vehicles, bonus, t = parse_input('data/a_example.in')
+    cars = [Car(i) for i in range(n_vehicles)]
+
+    for c in cars:
+        r = pick_ride(c, rides)
+        r.car = c
+
     print(rides[0])
