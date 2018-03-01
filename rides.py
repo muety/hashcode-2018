607bee6e932e8d565ae8f62e158a7b4fef3f7121
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-03-01 20:29:23 +0100

    Working state.

--- rides.py
+++ rides.py
@@ -3,17 +3,17 @@ class Car():
         self.id = id
         self.position = (0,0)
         self.rides = []
+        self.current_t = 0
 
 class Ride():
     def __init__(self, id, start_x, start_y, dest_x, dest_y, earliest, latest):
         self.id = id
-        self.start_x = start_x
-        self.start_y = start_y
-        self.dest_x = dest_x
-        self.dest_y = dest_y
-        self.earliest = earliest
-        self.latest = latest
-        self.completed = False
+        self.start_x = int(start_x)
+        self.start_y = int(start_y)
+        self.dest_x = int(dest_x)
+        self.dest_y = int(dest_y)
+        self.earliest = int(earliest)
+        self.latest = int(latest)
         self.car = None
         self.score = 0
 
@@ -25,13 +25,13 @@ def parse_input(file):
     with open(file, 'r') as f:
         for i, line in enumerate(f):
             if i == 0: rows, cols, n_vehicles, n_rides, bonus, t = line.split(' ')
-            rides.append(Ride(i, *(line.split(' '))))
+            else: rides.append(Ride(i - 1, *(line.split(' '))))
     return rides, int(rows), int(cols), int(n_vehicles), int(bonus), int(t)
 
 def dump_rides(file, cars):
     with open(file, 'w') as f:
         for c in cars:
-            f.write('{} '.format(c.id))
+            f.write('{} '.format(len(c.rides)))
             for r in c.rides:
                 f.write('{} '.format(r.id))
             f.write('\n')
@@ -40,20 +40,46 @@ def dump_rides(file, cars):
 def get_distance(start, end):
     return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
-def score_ride(car, ride):
-    return 1
+def get_distance_for_ride(ride):
+    return get_distance((ride.start_x, ride.start_y), (ride.dest_x, ride.dest_y))
 
-def pick_ride(car, rides):
-    filtered = [r for r in rides if not r.completed and r.car is None]
-    return sorted(filtered, key=lambda r: score_ride(car, r), reverse=True)[0]
+def count_steps(car, ride):
+    new_t = car.current_t
+    new_t += get_distance(car.position, (ride.start_x, ride.start_y))
+    new_t += max(0, ride.earliest - car.current_t)
+    new_t += get_distance((ride.start_x, ride.start_y), (ride.dest_x, ride.dest_y))
+    return new_t - car.current_t
+
+def score_ride(car, ride, bonus):
+    drive_distance = get_distance_for_ride(ride)
+    pick_distance = get_distance(car.position, (ride.start_x, ride.start_y))
+    wait_time = max(0, ride.earliest - (car.current_t + pick_distance))
+    on_time = pick_distance + car.current_t <= ride.earliest
+
+    # TODO: weights
+    return drive_distance - pick_distance - wait_time + (bonus if on_time else 0)
+
+def pick_ride(car, rides, t, bonus):
+    candidates = [r for r in rides if r.car is None]
+    candidates = [r for r in candidates if count_steps(car, r) < (t - car.current_t)]
+    candidates = [r for r in candidates if car.current_t + count_steps(car, r) <= r.latest]
+    candidates = sorted(candidates, key=lambda r: score_ride(car, r, bonus), reverse=True)
+    return candidates[0] if len(candidates) > 0 else None
 
 if __name__ == '__main__':
-    rides, rows, cols, n_vehicles, bonus, t = parse_input('data/a_example.in')
-    cars = [Car(i) for i in range(n_vehicles)]
+    rides, rows, cols, n_vehicles, bonus, t = parse_input('data/c_no_hurry.in')
+    cars = [Car(i + 1) for i in range(n_vehicles)]
 
     for c in cars:
-        r = pick_ride(c, rides)
-        r.car = c
-        c.rides.append(r)
+        r = pick_ride(c, rides, t, bonus)
+        can_pick = r is not None
+        while can_pick:
+            r = pick_ride(c, rides, t, bonus)
+            if r is None: break
+
+            r.car = c
+            c.rides.append(r)
+            c.current_t += count_steps(c, r)
+            c.position = (r.dest_x, r.dest_y)
 
     dump_rides('out.txt', cars)
